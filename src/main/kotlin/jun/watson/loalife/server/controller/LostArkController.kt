fa8abe85e730c4e8362b24ab3a8b99276a206116
kjoon418 <kjoon418@naver.com>
2025-06-04 09:19:41 +0900

    chore: 핸들링하지 못한 예외 발생시 API 응답 바디에 메시지를 담게 함

--- src/main/kotlin/jun/watson/loalife/server/controller/LostArkController.kt
+++ src/main/kotlin/jun/watson/loalife/server/controller/LostArkController.kt
@@ -8,11 +8,7 @@ import jun.watson.loalife.server.service.ExpeditionSearchService
 import jun.watson.loalife.server.service.ResourceService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
@@ -51,7 +47,7 @@ class LostArkController(
             is CharacterNotExistException -> {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.message)
             }
-            else -> throw exception
+            else -> return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.message)
         }
     }
 
